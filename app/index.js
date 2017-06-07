879d834160db75447ab513fafe491a5a2bed9964
byelipk <byelipk@gmail.com>
2017-06-07 17:19:32 -0400

    Load in jquery, tether, & bootstrap js/scss

---
+++ app/index.js
@@ -0,0 +1,5 @@
+import jQuery from "jquery/src/jquery.js";
+import Tether from "tether/dist/js/tether.min.js";
+import Alert  from "bootstrap/js/src/alert.js";
+
+import 'bootstrap/scss/bootstrap.scss';
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractCSS = new ExtractTextPlugin('styles.css');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        fallback: 'style-loader',
+        use: extractCSS.extract(['css-loader', 'sass-loader'])
+      }
+    ]
+  },
+  plugins: [
+    extractCSS
+  ]
+};
