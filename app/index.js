b712ef8873696d5cefc4bce032837eaa87e05871
byelipk <byelipk@gmail.com>
2017-06-21 08:37:02 -0400

    Remove typed plus lots of other fixes.

--- app/index.js
+++ app/index.js
@@ -3,28 +3,23 @@ import "toastr";
 
 const Toaster = require("toastr");
 
-console.log(Toaster);
-
 import "../scss/main.scss";
 
-import Typed from "./typed.js";
-
 document.addEventListener("DOMContentLoaded", function(){
-  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  var days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ];
   var d = new Date();
   var dayName = days[d.getDay()];
 
-  Typed.new(".contact h1", {
-    strings: [
-      "Looking for a frontend engineer?^1000",
-      "That's great!<br> I'm looking for a job. :) ^1000",
-      "Let's get in touch.^1000",
-      "Email me at:<br> <strong><a href='mailto:byelipk@gmail.com'>byelipk@gmail.com</a></strong>^3000<br><small>Just click the link</small>^8000",
-      `Have a great ${dayName}!^5000`
-    ],
-    typeSpeed: 50,
-    loop: true
-  });
+  let el = document.querySelector("#happy");
+  el.textContent = `${dayName}!`;
 
   $("#story").click(function() {
       $('html, body').animate({
@@ -32,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function(){
       }, 2000);
   });
 
-  $("#projects").click(function() {
+  $("#projects, #to-projects-from-contact").click(function() {
       $('html, body').animate({
           scrollTop: $(".projects").offset().top
       }, 2000);
